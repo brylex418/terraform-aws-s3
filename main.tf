d69c705172a8911bf32b84c3f731e99db4750ce0
brylex <brylex@amazon.com>
2020-01-03 15:34:39 -0700

    adding srr

--- main.tf
+++ main.tf
@@ -283,3 +283,103 @@ resource "aws_s3_bucket_policy" "s3_encryption" {
   bucket = aws_s3_bucket.s3_encryption[count.index].id
   policy = var.aws_iam_policy_document
 }
+
+# Module      : S3 SRR Replication
+# Description : Terraform module which configures SRR
+resource "aws_s3_bucket" "destination" {
+  bucket = "tf-test-bucket-destination-12345-brylex"
+  region = "us-east-1"
+
+  versioning {
+    enabled = var.versioning
+  }
+}
+
+resource "aws_s3_bucket" "source" {
+  bucket   = "tf-test-bucket-12345-brylex"
+  region   = "us-west-2"
+
+  versioning {
+    enabled = var.versioning
+  }
+
+ replication_configuration {
+    role = "${aws_iam_role.replication.arn}"
+
+    rules {
+      id     = "foobar"
+      prefix = "foo"
+      status = "Enabled"
+
+      destination {
+        bucket        = "${aws_s3_bucket.destination.arn}"
+        storage_class = "STANDARD"
+      }
+    }
+  }
+} 
+
+  resource "aws_iam_role" "replication" {
+  name = "tf-iam-role-replication-12345"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "s3.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_policy" "replication" {
+  name = "tf-iam-role-policy-replication-12345"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetReplicationConfiguration",
+        "s3:ListBucket"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_s3_bucket.bucket.arn}"
+      ]
+    },
+    {
+      "Action": [
+        "s3:GetObjectVersion",
+        "s3:GetObjectVersionAcl"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_s3_bucket.bucket.arn}/*"
+      ]
+    },
+    {
+      "Action": [
+        "s3:ReplicateObject",
+        "s3:ReplicateDelete"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_s3_bucket.destination.arn}/*"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "replication" {
+  role       = "${aws_iam_role.replication.name}"
+  policy_arn = "${aws_iam_policy.replication.arn}"
+}
